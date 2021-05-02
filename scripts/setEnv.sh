d8b23e99cda82efae868cbb22c4417eca6778239
mjamal14 <mjamal14@gmail.com>
2021-05-02 05:15:03 -0400

    Update setEnv.sh

--- scripts/setEnv.sh
+++ scripts/setEnv.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
-echo "Display Version11"
+#echo "Display Version11"
 
 #wget https://ci.roster.thomsonreuters.com/.env -O .env.ci
 
-#diff .env .env.ci
+chmod 777 scripts/var1 scripts/var2
+diff scripts/var1 scripts/var2 > /dev/null 2>&1
+error=$?
+if [ $error -eq 0 ]
+then
+   echo "var1 and $var2 are the same file"
+elif [ $error -eq 1 ]
+then
+   echo "var1 and var2 differ"
+else
+   echo "There was something wrong with the diff command"
+fi
 
 #sleep 10000
 
